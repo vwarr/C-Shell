9856e6bf8b132df26470316dd8921f7215127c6e
Varun Warrier <varunswarrier@gmail.com>
2024-11-25 13:19:54 -0500

    reading func

--- loop.c
+++ loop.c
@@ -1,5 +1,12 @@
-void lsh_loop() {
+#include <stdio.h>
+#include <read.c>
+
+void csh_loop() {
     char *line;
     char **args;
     int status; 
+    do {
+        printf(">");
+        line = csh_read_line();
+    } while (status);
 }
---
+++ read.c
@@ -0,0 +1,33 @@
+#define INITIAL_BUFFER_SIZE 1024
+#include <stdio.h>
+#include <stdlib.h>
+
+char *csh_read_line() {
+    int buf_size = INITIAL_BUFFER_SIZE;
+    int pos = 0;
+    char *buffer = (char*) malloc(sizeof(char) * buf_size);
+    int c;
+
+    if (!buffer) {
+        fprintf(stderr, "csh: memory allocation failure\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1) {
+        c = getchar();
+        if (c == '\0' || c == EOF) {
+            buffer[pos] = '\0';
+            return buffer;
+        } else {
+            buffer[pos] = c;
+        }
+        pos++;
+        if (pos >= buf_size) {
+            buffer = realloc(buffer, sizeof(char) * (buf_size + INITIAL_BUFFER_SIZE));
+            if (!buffer) {
+                fprintf(stderr, "csh: memory allocation failure\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
