ec517ee20e2200b71aaff2c9c1d9ff853191fc5d
Varun Warrier <varunswarrier@gmail.com>
2024-12-15 16:21:58 -0500

    execute

--- functionalities/execute.c
+++ functionalities/execute.c
@@ -1,5 +1,24 @@
-// #include <unistd.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-// int csh_launch(char**args) {
-//     pid_t
-// }
+int csh_launch(char **args) {
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        if (execvp(args[0], args) == -1){
+            perror("csh");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        perror("csh");
+    } else {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
